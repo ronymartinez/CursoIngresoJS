c3978eee0842f4ee643f4f52ea6f26cb995b1dc3
ronymartinez <60630330+ronymartinez@users.noreply.github.com>
2020-02-10 22:25:24 -0300

    Add files via upload
    clase 10/02

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
 
+if (edad >= 18) {
+    alert("Adulto");
+}
+else {
+    if (edad >= 13) {
+        alert("Adolescente");
 
+    }
+    else{
+        alert("Niño");
+    
+    }
+
+}
 
 }//FIN DE LA FUNCIÓN
