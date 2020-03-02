0c48cd45983facff643369c91e45ea7e691df1b5
ronymartinez <60630330+ronymartinez@users.noreply.github.com>
2020-03-02 17:41:54 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -3,10 +3,52 @@ function mostrar()
     var marca;
     var peso;
     var temperatura;
+    var marcaMasPesado;
     var respuesta="si";
+    var temperaturasPares;
+    var cantidadBajoCero;
+    var promedioPesoTotal;
+    var pesoMaximo;
+    var pesoMinimo;
+
+
+    while (respuesta=="si"){
+
+        marca=prompt("Ingrese la marca del producto");
+        console.log(marca)
+
+        peso=prompt("Ingrese el peso (entre 1 y 100)");
+        peso=parseInt(peso);
+        console.log(peso)
+
+        while (isNaN(peso) || peso<1 || peso>100){
+        peso=prompt("Error: reingrese el peso (entre 1 y 100)");
+        peso=parseInt(peso);
+
+        }
+
+        temperatura=prompt("Ingrese la temperatura");
+        temperatura=parseInt(temperatura);
+        console.log(temperatura)
+
+        while(isNaN(temperatura) || temperatura>30 || temperatura<-30){
+        temperatura=prompt("Error: reingrese temperatura entre -30 y 30")
+        
+        }
+
+        respuesta= prompt ("Si desea continuar ingrese 'si'");
+    }
+    document.write("-La cantidad de temperaturas pares es: " + temperaturasPares + "<br>"+
+                "-La marca del producto más pesado es: " + marcaMasPesado + "<br>"+
+                "-La cantidad de productos a menos de cero grados: " + cantidadBajoCero  + "<br>"+
+                "-El promedio del peso de los productos es de: " + promedioPesoTotal  + "<br>"+
+                "-El peso máximo y el mínimo es: " +pesoMaximo + " y " + pesoMinimo  + "<br>"
+    )
 
 
 
+
+    /*
     while(respuesta=="si"){
     
         marca= prompt("Ingrese marca");
@@ -22,5 +64,5 @@ function mostrar()
         temperatura=parseInt(temperatura);
 
     }
-
+*/
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -10,17 +10,29 @@ function mostrar()
     var temperaturaAnimalMasPesado;
     var maximoPesado=0;
     var flag = 0;
-    var contadorBajoCero;
+    var contadorBajoCero=0;
+    var contadorPeso=0;
+    var acumuladorPeso=0;
+    var promedioPeso;
+    var pesoMaximoBajoCero;
+    var pesoMinimoBajoCero;
     
     while(respuesta=="si"){
     
         animal= prompt("Ingrese animal");
-        
+        console.log(animal)        
 
         peso= prompt("Ingrese peso entre 1 y 1000")
         peso=parseInt(peso);
-        while (peso<1 || peso>1000){
+        contadorPeso++;
+        acumuladorPeso= acumuladorPeso + peso;
+
+        while (peso<1 || peso>1000 || isNaN(peso)){
+            peso= prompt("Error: Ingrese peso entre 1 y 1000")
             peso=parseInt(peso);}
+        console.log(peso)
+        
+       
 
         if (flag==0){
             maximoPesado=peso
@@ -32,37 +44,33 @@ function mostrar()
             nombreAnimalMasPesado=animal;
 
         }
-
-
         temperatura= prompt("Ingrese temperatura entre -30 y 30");
         temperatura=parseInt(temperatura);
-            while (temperatura<-30 || temperatura>30){
+            while (temperatura<-30 || temperatura>30 || isNaN(peso)) {
+            temperatura= prompt("Error: Ingrese temperatura entre -30 y 30");
             temperatura=parseInt(temperatura);}
-        if (resto=temperatura%2){
-            temperaturasPares++ }
-        
-        if (flag==0){
-            temperaturaAnimalMasPesado=temperatura;
-             flag=1
-            }
-            else if (temperatura>temperaturaAnimalMasPesado) {
-                temperaturaAnimalMasPesado= temperatura;
-            }
+            console.log(temperatura)
 
-        if (peso<0){
+        if (resto==temperatura%2){
+            temperaturasPares++ }
+
+        if (temperatura<0){
             contadorBajoCero++
         }
-
-
+        respuesta=prompt("Ingrese 'si' para continuar o 'no' para cancelar'");
     }
 
-    respuesta=prompt("Para continuar ingrese 'si'");
+    promedioPeso= acumuladorPeso / contadorPeso;
+    pesoMaximoBajoCero= //FALTA
+    pesoMinimoBajoCero= //FALTA
+    //FALTA Temperatura del animal más pesado.
 
     document.write("-Cantidad de temperaturas pares: " + temperaturasPares+ "<br>"+
-                "-Nombre y temperatura del animal más pesado: "+nombreAnimalMasPesado +"<br>"+
-                "-Cantidad de animales que viven a menos de 0 grados: " + contadorBajoCero +"<br>"
-            
-                    )
+                "-Nombre y temperatura del animal más pesado: "+nombreAnimalMasPesado + " y " +temperaturaAnimalMasPesado + " grados" +"<br>"+
+                "-Cantidad de animales que viven a menos de 0 grados: " + contadorBajoCero +"<br>"+
+                "-Promedio del peso de todos los animales: " + promedioPeso +"<br>"+
+                "-Peso máximo y mínimo de animales que viven bajo cero: " + pesoMaximoBajoCero + " y " + pesoMinimoBajoCero + "<br>"
+         )
     
 
 }
